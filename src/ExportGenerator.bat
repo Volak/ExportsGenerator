1fdb631d17dae392345578649efd4874c1832596
Volak <81952316+Volak@users.noreply.github.com>
2025-01-11 10:37:17 -0600

    Add files via upload

---
+++ src/ExportGenerator.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Get the directory of the batch file
+set "SCRIPT_DIR=%~dp0"
+
+REM Navigate to the script directory
+cd /d "%SCRIPT_DIR%"
+
+REM Run the Python script
+python ExportGenerator.py
+
+REM Pause the console after execution
+pause
